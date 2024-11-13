c151a65e67f7f0c95c4413aa12a38e8e9bf932fd
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-11-12 21:44:36 -0300

    feat: add initial routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/list-books/list-books.component').then(
+        (c) => c.ListBooksComponent
+      ),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./components/book-detail/book-detail.component').then(
+        (c) => c.BookDetailComponent
+      ),
+  },
+  {
+    path: 'capitulo/:book/:chapter',
+    loadComponent: () =>
+      import('./components/chapter-detail/chapter-detail.component').then(
+        (c) => c.ChapterDetailComponent
+      ),
+  },
+];
