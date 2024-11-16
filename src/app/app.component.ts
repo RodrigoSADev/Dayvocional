4fc8f88550857bc6c7c535a03d9a049ddb667d18
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-11-16 13:29:27 -0300

    feat: create header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { A11yComponent } from './components/a11y/a11y.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, A11yComponent],
+  imports: [RouterOutlet, HeaderComponent, A11yComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
