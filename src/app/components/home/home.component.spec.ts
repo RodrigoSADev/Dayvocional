986bfb0c2fca3f707dbb3d777d14e059a02a4d08
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-11-18 15:03:43 -0300

    feat: added random verse structure

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,5 +1,5 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,9 +8,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      imports: [HomeComponent],
+      providers: [provideHttpClient],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { IVerseDetail } from '../../interfaces/book.interface';
+import { BibleService } from '../../services/bible.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  bibleService = inject(BibleService);
 
+  randomVerse = signal<IVerseDetail | null>(null);
+  randomVerseBook = signal<IVerseDetail | null>(null);
+  books = signal([
+    'gn',
+    'ex',
+    'lv',
+    'nm',
+    'dt',
+    'js',
+    'jz',
+    'rt',
+    '1sm',
+    '2sm',
+    '1rs',
+    '2rs',
+    '1cr',
+    '2cr',
+    'ed',
+    'ne',
+    'et',
+    'job',
+    'sl',
+    'pv',
+    'ec',
+    'ct',
+    'is',
+    'jr',
+    'lm',
+    'ez',
+    'dn',
+    'os',
+    'jl',
+    'am',
+    'ob',
+    'jn',
+    'mq',
+    'na',
+    'hc',
+    'sf',
+    'ag',
+    'zc',
+    'ml',
+    'mt',
+    'mc',
+    'lc',
+    'jo',
+    'at',
+    'rm',
+    '1co',
+    '2co',
+    'gl',
+    'ef',
+    'fp',
+    'cl',
+    '1ts',
+    '2ts',
+    '1tm',
+    '2tm',
+    'tt',
+    'fm',
+    'hb',
+    'tg',
+    '1pe',
+    '2pe',
+    '1jo',
+    '2jo',
+    '3jo',
+    'jd',
+    'ap',
+  ]);
+
+  ngOnInit(): void {
+    this.bibleService.getRandomVerse('nvi').subscribe((resp) => {
+      this.randomVerse.set(resp);
+    });
+    this.getRandomVerseAleatoryBook();
+  }
+
+  getRandomVerseAleatoryBook() {
+    this.bibleService
+      .getRandomVerseBook(
+        'nvi',
+        this.books()[Math.floor(Math.random() * this.books().length)]
+      )
+      .subscribe((resp) => {
+        this.randomVerseBook.set(resp);
+      });
+  }
 }
