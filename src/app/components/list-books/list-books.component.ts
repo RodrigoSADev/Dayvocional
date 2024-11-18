607aa2a43443c3b3c15b1b7c463ad9e5d037f865
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2024-11-18 11:58:56 -0300

    feat: group books by category in Bible list and capitalize group names in Bible list

--- src/app/components/list-books/list-books.component.ts
+++ src/app/components/list-books/list-books.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IBook } from '../../interfaces/book.interface';
@@ -6,7 +7,7 @@ import { BibleService } from '../../services/bible.service';
 @Component({
   selector: 'app-list-books',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './list-books.component.html',
   styleUrl: './list-books.component.scss',
 })
@@ -14,10 +15,34 @@ export class ListBooksComponent implements OnInit {
   bibleService = inject(BibleService);
 
   books = signal<IBook[]>([]);
+  groupedBooks = signal<{ name: string; books: IBook[] }[]>([]);
 
   ngOnInit(): void {
     this.bibleService.getBooks().subscribe((resp) => {
       this.books.set(resp);
+      this.groupBooksByGroup();
     });
+  }
+
+  groupBooksByGroup(): void {
+    const grouped = this.books().reduce((acc, book) => {
+      const groupName = this.capitalizeWords(book.group);
+      let group = acc.find((group) => group.name === groupName);
+      if (!group) {
+        group = { name: groupName, books: [] };
+        acc.push(group);
+      }
+      group.books.push(book);
+      return acc;
+    }, [] as { name: string; books: IBook[] }[]);
+
+    this.groupedBooks.set(grouped);
+  }
+
+  capitalizeWords(text: string): string {
+    return text
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
   }
 }
